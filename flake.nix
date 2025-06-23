1c95cf72d886c8b735ab7b86bde0dbbcf0d26fd3
omeyemburg <oskar.git@gmx.net>
2025-06-23 11:14:25 +0200

    chore(nix): add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment with OpenGL and SDL";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            SDL2
+            mesa
+            rustup
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.bashInteractive}/bin/bash
+          '';
+        };
+      }
+    );
+}
