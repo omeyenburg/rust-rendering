668f8c46153be861c14df940159228103259af85
omeyemburg <oskar.git@gmx.net>
2025-06-23 11:02:08 +0200

    feat: config & data directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![allow(dead_code, unused_imports)]
+mod window;    // Used A) to introduce window into scope and B) to allow the path "window::greet"
+use window::*; // Used for "NUMBER" without "hello::"
+
+extern crate directories;
+use directories::ProjectDirs;
+
+enum State {
+    Menu,
+    Game,
+    // ...
+}
+
+fn game() {
+    let var = "Hello, world!";
+    println!("{}", var);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Directories used: config_dir for options; data_dir for other data (!!!paths are identical on macos!!!)
+    if let Some(proj_dirs) = ProjectDirs::from("", "",  "rust-rendering") {
+        println!("Config dir: {:?}", proj_dirs.config_dir());
+        println!("Data dir: {:?}", proj_dirs.data_dir());
+    }
+
+    let state = State::Game;
+    match state {
+        State::Game => game(),
+        State::Menu => ()
+    }
 }
